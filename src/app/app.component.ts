4f85a9017b4f266f1472119e9a71f70a55250325
Deepak Bisht <deepsluminia800@gmail.com>
2019-04-11 15:25:58 +0530

    Images Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstAngularApp';
-  count:number = 0;
-  url:string = '';
-  counter(){
+  count: number = 0;
+  url: string = 'https://images.pexels.com/photos/2089799/pexels-photo-2089799.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+  width: number = 500;
+  height: number = 400;
+  counter() {
     this.count++;
   }
-  updateImg(){
-    this.url='';
+  updateImg() {
+    if (this.url.indexOf('2089799') != -1) {
+      this.url = 'https://images.pexels.com/photos/1054218/pexels-photo-1054218.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+    } else {
+      this.url = 'https://images.pexels.com/photos/2089799/pexels-photo-2089799.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+    }
   }
 }
