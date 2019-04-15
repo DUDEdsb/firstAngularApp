353b4d7e119c598f7931e311dc28a0c6d108ff84
Deepak Singh Bisht <deepsluminia800@gmail.com>
2019-04-15 11:13:11 +0530

    index and topbar

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent implements OnInit {
- title: String = 'Facebook';
+ title: String = 'fakebook';
   constructor() { }
 
   ngOnInit() {
